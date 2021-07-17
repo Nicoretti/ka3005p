0ee6dedd0e1c08ead70719b47aebd7ac69f6c54c
Jack <Jack.Newman12@gmail.com>
2021-07-17 16:21:13 +1000

    Add ability to list devices and manually specifiy one

--- src/bin/ka3005p.rs
+++ src/bin/ka3005p.rs
@@ -2,12 +2,36 @@
 use anyhow::Context;
 use std::convert::TryInto;
 use std::io::BufRead;
+use std::process::exit;
 use structopt::StructOpt;
 
 fn main() -> ::anyhow::Result<(), anyhow::Error> {
     human_panic::setup_panic!();
     let args = ka3005p::cli::Ka3005p::from_args();
-    let mut serial = ka3005p::find_serial_port()?;
+
+    if let ka3005p::cli::Command::List{verbose} = args.command {
+        let devices;
+        if verbose {
+            // Verbose. List everything
+            devices = serialport::available_ports()?;
+        } else {
+            // Just print devices and then exit
+            devices = ka3005p::list_serial_ports();
+        }
+
+        println!("{:?}", devices);
+        exit(0);
+    }
+
+    let mut serial;
+    if let Some(device) = args.device {
+        // User specified a device. Use that
+        serial = ka3005p::Ka3005p::new(&device)?;
+    } else {
+        // Otherwise find the device automatically
+        serial = ka3005p::find_serial_port()?;
+    }
+
     match args.command {
         ka3005p::cli::Command::Status => {
             println!("{}", serial.status()?);
@@ -36,5 +60,5 @@ fn main() -> ::anyhow::Result<(), anyhow::Error> {
             )?;
         }
     };
-    std::process::exit(0);
+    exit(0);
 }
